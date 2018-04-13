3c0e52dd1a9085262c2f3bbbe783413c8fcc35f9
jianxun <medusa.leee@gmail.com>
2018-04-13 11:37:51 +0800

    更换tomcat为jetty

--- src/main/java/com/viathink/flowable/SpringBootFlowableApplication.java
+++ src/main/java/com/viathink/flowable/SpringBootFlowableApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableAutoConfiguration
 @EnableTransactionManagement
 @EnableWebMvc
+@EnableAsync
 public class SpringBootFlowableApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootFlowableApplication.class, args);
